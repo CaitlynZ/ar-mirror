95434c0a43bb7a131a11dfebe1ea36fa82991abb
cassie0508 <1365192522@qq.com>
2023-10-17 10:52:30 +0200

    add hide function, re-organize timeManager

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -8,9 +8,10 @@ public class CountdownTimer : MonoBehaviour
 {
     public float initialCountdownTime = 31.0f;
     public TextMeshProUGUI textMeshPro;
+    public Image backImage;
     public Image fillImage;
-    public AudioClip tickSound;
-    public AudioSource audioSource;
+    // public AudioClip tickSound;
+    // public AudioSource audioSource;
     private float countdownTime;
     private bool isPaused = false;
 
@@ -18,43 +19,50 @@ public class CountdownTimer : MonoBehaviour
     {
         // textMeshPro = GetComponent<TextMeshProUGUI>();
         countdownTime = initialCountdownTime;
-        UpdateCountdownText();
+        UpdateCountdown();
     }
 
    private void Update()
-    {        
+    {   
         if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Alpha5)){
             countdownTime = initialCountdownTime;
-            UpdateCountdownText();
+            textMeshPro.gameObject.SetActive(true);
+            fillImage.gameObject.SetActive(true);
+            backImage.gameObject.SetActive(true);
+            UpdateCountdown();
         }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
-            if (isPaused)
-            {
-                isPaused = false;
-            }
-            else
-            {
-                isPaused = true;
-            }
+            isPaused = !isPaused;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0)) {
+            HideCountdown();
         }
 
         if (countdownTime <= 0) {
             countdownTime = 0;
-            UpdateCountdownText();
+            UpdateCountdown();
         }
         else if (!isPaused) {
             countdownTime -= Time.deltaTime;
             // audioSource.PlayOneShot(tickSound);
-            UpdateCountdownText();
+            UpdateCountdown();
         }        
     }
 
-    private void UpdateCountdownText()
+    private void UpdateCountdown()
     {
         int seconds = Mathf.FloorToInt(countdownTime);
         textMeshPro.text = seconds.ToString();
         float fillAmount = countdownTime / initialCountdownTime;
         fillImage.fillAmount = fillAmount;
+    }
+
+    private void HideCountdown()
+    {
+        textMeshPro.gameObject.SetActive(false);
+        fillImage.gameObject.SetActive(false);
+        backImage.gameObject.SetActive(false);
     }
 }
